2d7c07f2b50d1e150b02ac6bfbecff0221e244cb
goddemi <goddemi0124@gmail.com>
2022-12-10 23:04:54 +0900

    Add : file Path

--- get-static-props/pages/index.js
+++ get-static-props/pages/index.js
@@ -1,4 +1,7 @@
-function HomePage({ product }) {
+import path from "path";
+import fs from "fs/promises";
+
+export function HomePage({ product }) {
   return (
     <ul>
       <li>{product.id}</li>
@@ -9,6 +12,9 @@ function HomePage({ product }) {
 }
 
 export const getStaticProps = async () => {
+  const filePath = path.join();
+  await fs.readFile();
+
   return {
     props: {
       product: [{ id: "hi", title: "me" }],
